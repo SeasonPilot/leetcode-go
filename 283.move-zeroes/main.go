a0562c4e1e6fcb3d19d8b28e272d70099384de3a
SeasonPilot <pilot1@foxmail.com>
2022-05-07 21:38:18 +0800

    rename

--- 283.move-zeroes/main.go
+++ 283.move-zeroes/main.go
@@ -9,12 +9,12 @@ func main() {
 
 // 所谓要把0移动到数组后面，其实就是把非0数给移动到数组前面，而每个非0数需要移动的步数其实就是这个非0数前面0的个数。
 func moveZeroes(nums []int) {
-	count := 0   // 记录当前 0 的个数
+	count := 0 // 记录当前 0 的个数
 	for i, num := range nums {
 		if num == 0 {
 			count++
 		} else {
-			nums[i-count] = nums[i]   // 非 0 数需要移动的步数其实就是这个非 0 数前面 0 的个数。
+			nums[i-count] = nums[i] // 非 0 数需要移动的步数其实就是这个非 0 数前面 0 的个数。
 		}
 	}
 
