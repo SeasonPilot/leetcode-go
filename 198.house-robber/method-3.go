f968bfebed59877f5683d168bf90e24ed4589bc6
SeasonPilot <pilot1@foxmail.com>
2022-05-09 22:31:14 +0800

    feat: 198.house-robber 使用滚动数组  优化代码 变量平移

--- 198.house-robber/method-3.go
+++ 198.house-robber/method-3.go
@@ -12,12 +12,14 @@ func rob3(nums []int) int {
 	}
 	second := max(first, nums[1])
 
-	var dp int
 	for i := 2; i < n; i++ {
-		dp = max(first+nums[i], second) // 这里不是 nums[2]
-		first = second
-		second = dp
+		//dp = max(first+nums[i], second) // 这里不是 nums[2]
+		//first = second
+		//second = dp
+
+		// 优化后 变量平移
+		first, second = second, max(first+nums[i], second)
 	}
 
-	return dp
+	return second
 }
