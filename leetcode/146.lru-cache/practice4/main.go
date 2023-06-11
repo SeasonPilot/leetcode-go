3ab26275d3f5b8ce414efa3e5e1cec6384b6609b
SeasonPilot <pilot1@foxmail.com>
2023-06-11 16:33:29 +0800

    feat: 146.lru-cache/practice4

---
+++ leetcode/146.lru-cache/practice4/main.go
@@ -0,0 +1,86 @@
+package practice4
+
+type DLinkedNode struct {
+	key   int
+	value int
+	prev  *DLinkedNode
+	next  *DLinkedNode
+}
+
+type LRUCache struct {
+	capacity int
+	size     int
+	cache    map[int]*DLinkedNode
+	head     *DLinkedNode
+	tail     *DLinkedNode
+}
+
+func Constructor(capacity int) LRUCache {
+	l := LRUCache{
+		capacity: capacity,
+		cache:    make(map[int]*DLinkedNode),
+		head:     &DLinkedNode{},
+		tail:     &DLinkedNode{},
+	}
+	l.head.next = l.tail
+	l.tail.prev = l.head
+
+	return l
+}
+
+func (l *LRUCache) Get(key int) int {
+	if node, ok := l.cache[key]; !ok {
+		return -1
+	} else {
+		l.moveToHead(node)
+		return node.value
+	}
+}
+
+func (l *LRUCache) Put(key int, value int) {
+	if node, ok := l.cache[key]; ok {
+		node.value = value
+		l.moveToHead(node)
+	} else {
+		newNode := &DLinkedNode{
+			key:   key,
+			value: value,
+			//prev:  &DLinkedNode{},//fixme: 不需要初始化，l.addToHead(newNode) 不会访问 newNode.prev，只会给 newNode.prev 赋值
+			//next:  &DLinkedNode{},
+		}
+
+		l.addToHead(newNode)
+		l.cache[key] = newNode
+		l.size++
+
+		if l.size > l.capacity {
+			removed := l.removeTail()
+			delete(l.cache, removed.key) //fixme: removed.key
+			l.size--
+		}
+	}
+}
+
+func (l *LRUCache) moveToHead(node *DLinkedNode) {
+	l.addToHead(node)
+	l.removeNode(node)
+}
+
+func (l *LRUCache) addToHead(node *DLinkedNode) {
+	node.next = l.head.next // 将新节点的next指向head的下一个节点
+	l.head.next.prev = node // 将head的下一个节点的prev指向新节点
+
+	node.prev = l.head // 将新节点的prev指向head
+	l.head.next = node // 将head的next指向新节点
+}
+
+func (l *LRUCache) removeTail() *DLinkedNode {
+	removed := l.tail.prev //
+	l.removeNode(removed)
+	return removed
+}
+
+func (l *LRUCache) removeNode(node *DLinkedNode) {
+	node.prev.next = node.next
+	node.next.prev = node.prev
+}
---
+++ leetcode/146.lru-cache/practice4/main_test.go
@@ -0,0 +1,46 @@
+package practice4
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+func TestLRUCache_Put(t *testing.T) {
+	type fields struct {
+		size     int
+		capacity int
+		cache    map[int]*DLinkedNode
+		head     *DLinkedNode
+		tail     *DLinkedNode
+	}
+	type args struct {
+		key   int
+		value int
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+	}{
+		{
+			name: "put1",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// ["LRUCache","put","put","get","put","put","get"]
+			// [[2],[2,1],[2,2],[2],[1,1],[4,1],[2]]
+			lru := Constructor(2)
+			lru.Put(2, 1)
+			lru.Put(2, 2)
+			//fmt.Println(lru.Get(2))
+			fmt.Printf("【结果是否正确】:%v \n", reflect.DeepEqual(2, lru.Get(2)))
+
+			lru.Put(1, 1)
+			lru.Put(4, 1)
+			//fmt.Println(lru.Get(2))
+			fmt.Printf("【结果是否正确】:%v \n", reflect.DeepEqual(-1, lru.Get(2)))
+		})
+	}
+}
